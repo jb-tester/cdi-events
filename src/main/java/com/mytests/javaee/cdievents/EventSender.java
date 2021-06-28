89b1e03a2759bf8d91cd004515f2e052da5450b8
irina.petrovskaya <irina.petrovskaya@jetbrains.com>
2021-06-28 17:39:29 +0300

    cleanup

--- src/main/java/com/mytests/javaee/cdievents/EventSender.java
+++ src/main/java/com/mytests/javaee/cdievents/EventSender.java
@@ -2,11 +2,7 @@ package com.mytests.javaee.cdievents;
 
 import java.util.concurrent.CompletionStage;
 
-/**
- * @author Radim Hanus
- * @author Arun Gupta
- * @author Arjan Tijms
- */
+
 public interface EventSender {
 
     void sendSync(String message);
